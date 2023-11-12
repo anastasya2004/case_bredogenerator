589cd017a1ac3d52803bc706ac1adf350ad9c0ef
angelinamakeeva <125530626+angelinamakeeva@users.noreply.github.com>
2023-11-12 20:49:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import random
+import string
 
-text = 'Метель в поле страшная. Колкий снег бьёт и колет всё живое. Но в лесу снег рыхлый и от собаки даже проваливается. Сквозь метель Жулька увидела летящую птичку и со всех ног бросилась за ней по снегу. Птичка плавно порхала, поднималась вверх и вертелась в волнах ветра. Она догнала, схватила, но это была не птичка, а старый сухой дубовый лист. Но ничего! Вот другой летит, и собака уже бежит за ним'
+with open('text.txt', 'r+') as file:
+    text = file.read()
+#text = 'Метель в поле страшная. Колкий снег бьёт и колет всё живое. Но в лесу снег рыхлый и от собаки даже проваливается. Сквозь метель Жулька увидела летящую птичку и со всех ног бросилась за ней по снегу. Птичка плавно порхала, поднималась вверх и вертелась в волнах ветра. Она догнала, схватила, но это была не птичка, а старый сухой дубовый лист. Но ничего! Вот другой летит, и собака уже бежит за ним'
 #text = input()
 
-lst_word = text.split()
+    lst_word = text.split()
 
-pairs = []
-for i in range(len(lst_word) - 1):
-    pairs.append([lst_word[i], lst_word[i+1]])
+    pairs = []
+    for i in range(len(lst_word) - 1):
+        pairs.append([lst_word[i], lst_word[i+1]])
 
-dict_word = {}
-for i in range(len(pairs)):
-    w1 = pairs[i][0]
-    w2 = pairs[i][1]
-    if w1 in dict_word.keys():
-        dict_word[w1].append(w2)
-    else:
-        dict_word[w1] = [w2]
+    dict_word = {}
+    for i in range(len(pairs)):
+        w1 = pairs[i][0]
+        w2 = pairs[i][1]
+        if w1 in dict_word.keys():
+            dict_word[w1].append(w2)
+        else:
+            dict_word[w1] = [w2]
 
-def create_delusion(dict_word, first_word, numbers_words):
-    word = first_word
-    text_delusion = [word]
-    for x in range(numbers_words - 1):
-        next_word = random.choice(dict_word[word])
-        text_delusion.append(next_word)
-        word = next_word
-    return ' '.join(text_delusion)
 
+    def create_delusion(dict_word, first_word, numbers_words):
+        word = first_word
+        text_delusion = [word.capitalize()]
+        for x in range(numbers_words - 1):
+            next_word = random.choice(dict_word[word])
+            text_delusion.append(next_word)
+            word = next_word.rstrip(string.punctuation)
+        return ' '.join(text_delusion)
+
+    first_word = random.choice(lst_word)
+    numbers_words = int(input("Выберите количество слов бреда: "))
+    print(create_delusion(dict_word, first_word, numbers_words))
+    file.seek(0)
+    file.write(create_delusion(dict_word, first_word, numbers_words))
+    file.truncate()
